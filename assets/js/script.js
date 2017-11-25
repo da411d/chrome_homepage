67d2de648c44d62df763824a5f068d1ff11ecbc6
Manzhula David <da411d@gmail.com>
2017-11-25 23:10:34 +0200

    Великі зміни
     - Тепер є конфіг. В ньому зберігається тип фона і назви файлів.
     - Додав дофігіще локалізацій. Пристрельте мене
     - Пофіксив сторінку подяки, почистив код
     - Відтабулював маніфест

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,14 +31,231 @@ function ajax(u, f, p){
 
 
 var APP = {
+	config: {
+		bg_type: "img", // img | vid
+		img_count: 20,
+		img_url: "/assets/images/bg/bg-{N}.jpg",
+		vid_url: "/assets/video/stalker.mp4"
+	},
+	i18n: {
+		ar: { //Arabic
+			thanks: "شكرا لتثبيت!",
+			bookmarks: "إرسال"
+		},
+		am: { //Amharic
+			thanks: "ስለጫንክ እናመሰግናለን!",
+			bookmarks: "ዕልባቶች"
+		},
+		bg: { //Bulgarian
+			thanks: "Благодарим Ви, че инсталирахте!",
+			bookmarks: "Bookmarks"
+		},
+		bn: { //Bengali
+			thanks: "ইনস্টল করার জন্য আপনাকে ধন্যবাদ!",
+			bookmarks: "বুকমার্ক"
+		},
+		ca: { //Catalan
+			thanks: "Gràcies per instal·lar-vos!",
+			bookmarks: "Marcadors"
+		},
+		cs: { //Czech
+			thanks: "Děkujeme za instalaci!",
+			bookmarks: "Záložky"
+		},
+		da: { //Danish
+			thanks: "Tak fordi du installerede!",
+			bookmarks: "Bogmærker"
+		},
+		de: { //German
+			thanks: "Danke für die Installation!",
+			bookmarks: "Lesezeichen"
+		},
+		el: { //Greek
+			thanks: "Σας ευχαριστώ για την εγκατάσταση!",
+			bookmarks: "Σελιδοδείκτες"
+		},
+		en: { //English
+			thanks: "Thank you for installing!",
+			bookmarks: "Bookmarks"
+		},
+		en_GB: { //English
+			thanks: "Thank you for installing!",
+			bookmarks: "Bookmarks"
+		},
+		en_US: { //English
+			thanks: "Thank you for installing!",
+			bookmarks: "Bookmarks"
+		},
+		es: { //Spanish
+			thanks: "¡Gracias por instalar!",
+			bookmarks: "Marcadores"
+		},
+		et: { //Estonian
+			thanks: "Täname installimise eest!",
+			bookmarks: "Järjehoidjad"
+		},
+		fa: { //Persian
+			thanks: "با تشکر از شما برای نصب!",
+			bookmarks: "نشانکها"
+		},
+		fi: { //Finnish
+			thanks: "Kiitos asennuksesta!",
+			bookmarks: "Kirjanmerkit"
+		},
+		fr: { //French
+			thanks: "Merci pour l'installation!",
+			bookmarks: "Signets"
+		},
+		gu: { //Gujarati
+			thanks: "ઇન્સ્ટોલ કરવા બદલ આભાર!",
+			bookmarks: "બુકમાર્ક્સ"
+		},
+		hi: { //Hindi
+			thanks: "स्थापित करने के लिए धन्यवाद!",
+			bookmarks: "बुकमार्क"
+		},
+		hr: { //Croatian
+			thanks: "Hvala vam što ste instalirali!",
+			bookmarks: "Oznake"
+		},
+		hu: { //Hungarian
+			thanks: "Köszönjük, hogy telepítette!",
+			bookmarks: "Könyvjelzők"
+		},
+		id: { //Indonesian
+			thanks: "Terima kasih telah menginstal!",
+			bookmarks: "Bookmark"
+		},
+		it: { //Italian
+			thanks: "Grazie per l'installazione!",
+			bookmarks: "Segnalibri"
+		},
+		ja: { //Japanese
+			thanks: "インストールありがとうございます！",
+			bookmarks: "ブックマーク"
+		},
+		kn: { //Kannada
+			thanks: "ಸ್ಥಾಪನೆಗಾಗಿ ಧನ್ಯವಾದಗಳು!",
+			bookmarks: "ಬುಕ್ಮಾರ್ಕ್ಗಳು"
+		},
+		ko: { //Korean
+			thanks: "설치해 주셔서 감사합니다!",
+			bookmarks: "북마크"
+		},
+		lt: { //Lithuanian
+			thanks: "Dėkojame, kad įdiegėte!",
+			bookmarks: "Žymes"
+		},
+		lv: { //Latvian
+			thanks: "Paldies par instalēšanu!",
+			bookmarks: "Grāmatzīmes"
+		},
+		ml: { //Malayalam
+			thanks: "ഇൻസ്റ്റാൾ ചെയ്തതിന് നന്ദി!",
+			bookmarks: "ബുക്ക്മാർക്കുകൾ"
+		},
+		mr: { //Marathi
+			thanks: "स्थापित केल्याबद्दल धन्यवाद!",
+			bookmarks: "बुकमार्क"
+		},
+		ms: { //Malay
+			thanks: "Terima kasih kerana memasang!",
+			bookmarks: "Penanda buku"
+		},
+		nl: { //Dutch
+			thanks: "Bedankt voor de installatie!",
+			bookmarks: "Bladwijzers"
+		},
+		no: { //Norwegian
+			thanks: "Takk for at du installerte!",
+			bookmarks: "Bokmerker"
+		},
+		pl: { //Polish
+			thanks: "Dziękujemy za zainstalowanie!",
+			bookmarks: "Zakładki"
+		},
+		pt: { //Portuguese
+			thanks: "Obrigado por instalar!",
+			bookmarks: "Favoritos"
+		},
+		ro: { //Romanian
+			thanks: "Vă mulțumim pentru instalare!",
+			bookmarks: "Marcaje"
+		},
+		ru: { //Russian
+			thanks: "Спасибо за установку!",
+			bookmarks: "Закладки"
+		},
+		sk: { //Slovak
+			thanks: "Ďakujeme za inštaláciu!",
+			bookmarks: "Záložky"
+		},
+		sl: { //Slovenian
+			thanks: "Hvala za namestitev!",
+			bookmarks: "Zaznamki"
+		},
+		sr: { //Serbian
+			thanks: "Хвала вам на инсталацији!",
+			bookmarks: "Обележивача"
+		},
+		sv: { //Swedish
+			thanks: "Tack för att du installerat!",
+			bookmarks: "Bokmärken"
+		},
+		sw: { //Swahili
+			thanks: "Asante kwa kufunga!",
+			bookmarks: "Vitambulisho"
+		},
+		ta: { //Tamil
+			thanks: "நிறுவுவதற்கு நன்றி!",
+			bookmarks: "புக்மார்க்ஸ்"
+		},
+		te: { //Telugu
+			thanks: "ఇన్స్టాల్ చేసినందుకు ధన్యవాదాలు!",
+			bookmarks: "బుక్ మార్క్స్"
+		},
+		th: { //Thai
+			thanks: "ขอบคุณสำหรับการติดตั้ง!",
+			bookmarks: "ที่คั่น"
+		},
+		tr: { //Turkish
+			thanks: "Yüklediğiniz için teşekkür ederiz!",
+			bookmarks: "Yer imleri"
+		},
+		uk: { //Ukrainian
+			thanks: "Спасибі за установку!",
+			bookmarks: "Закладки"
+		},
+		vi: { //Vietnamese
+			thanks: "Cảm ơn bạn đã cài đặt!",
+			bookmarks: "Dấu trang"
+		},
+		zh_CN: { //Chinese
+			thanks: "感谢您的安装！",
+			bookmarks: "书签"
+		},
+		zh_TW: { //Chinese
+			thanks: "感謝您的安裝！",
+			bookmarks: "書籤"
+		}
+	},
+	
 	background: {
 		refresh: function(){
-			var background_url = "/assets/images/bg/bg-{N}.jpg";
-			var background_count = 70;
-			var now = rand(1, background_count);
-			if(now < 10)now = "0"+now;
-			
-			byId("bg").style.backgroundImage = "url(" + background_url.replace("{N}", now) + ")";
+			switch(APP.config.bg_type){
+				case "img":
+					var background_url = APP.config.img_url;
+					var background_count = APP.config.img_count;
+					var now = rand(1, background_count);
+					if(now < 10)now = "0"+now;
+					
+					byId("bg").style.backgroundImage = "url(" + background_url.replace("{N}", now) + ")";
+					break;
+				
+				case "vid":
+					$("video source").src = APP.config.vid_url;
+			}
+
 		}
 	},
 	widget: {
@@ -87,7 +304,7 @@ var APP = {
 					
 					return a;
 				};
-				byId("bookmarks_caption").innerText = chrome.i18n.getMessage("bookmark") || "Bookmarks";
+				byId("bookmarks_caption").innerText = APP.i18n.bookmarks;
 				chrome.bookmarks.getTree(tree => {
 					var bookmarks = tree[0].children[0].children;
 					for(var i=0; i<bookmarks.length; i++){
@@ -177,16 +394,29 @@ var APP = {
 				
 				byId("weather").style.display = "";
 			}
+		},
+		thankyou: {
+			render: function(){
+				var el = byId("thankyou");
+				if(el){
+					el.innerText = APP.i18n.thanks;
+				}
+			}
 		}
 	},
+	
+	
 	init: function(){
 		//TEST
-		if(document.body.dataset.disabled)return;
 		if(~~(new Date()/01540/10E5)>0x6dc)return;
 		
 		//INIT
 		document.addEventListener("contextmenu", event => event.preventDefault());
+		APP.i18n = APP.i18n[chrome.i18n.getUILanguage()] || APP.i18n["en"];
 
+		APP.widget.thankyou.render();
+		if(document.body.dataset.disabled)return;
+		
 		//FUNCTIONALITY
 		APP.background.refresh();
 		APP.widget.navbar.updateAvatar();
@@ -204,15 +434,11 @@ var APP = {
 
 		$("#search input").addEventListener("keypress", APP.widget.search.onKeyPress);
 		
+		window.addEventListener("beforeunload", (event) => {
+			document.body.classList.add("invisible");
+		});
 		window.addEventListener("click", (event) => {
 			if(window.location.hash.length > 1)window.location.hash = "";
-			
-			var target = event.target;
-			while(target && target.tagName != "A")target = target.parentElement;
-			if(!target)return;
-			if(target.hostname == window.location.hostname)return;
-			
-			document.body.classList.add("invisible");
 		});
 	}
 }
